3d865f99850cb582a0f77e8794c0605f124e7198
Gustavo Kaash <gtf23@hotmail.com>
2020-02-12 22:16:51 -0300

    Update home_page.dart

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -57,6 +57,7 @@ class _HomePageState extends State<HomePage> {
               onSubmitted: (text){
                 setState(() {
                   _search = text;
+                  _offset = 0;
                 });
               },
             )
