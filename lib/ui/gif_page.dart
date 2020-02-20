c4eb0b41b3a96d6407a915d2f0eaad3851d3c214
Gustavo Kaash <gtf23@hotmail.com>
2020-02-20 20:21:02 -0300

    Update gif_page.dart

--- lib/ui/gif_page.dart
+++ lib/ui/gif_page.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class GifPage extends StatelessWidget {
 
-final Map _GifData;
-GifPage(this._GifData);
+final Map _gifdata;
+GifPage(this._gifdata);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_GifData["title"]),
+        title: Text(_gifdata["title"]),
         backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: (){
+                Share.share(_gifdata["images"]["fixed_height"]["url"]);
+            },
+          ),
+        ],
       ),
       backgroundColor: Colors.black,
       body: Center(
-        child: Image.network(_GifData["images"]["fixed_height"]["url"])
+        child: Image.network(_gifdata["images"]["fixed_height"]["url"])
       )
     );
   }
